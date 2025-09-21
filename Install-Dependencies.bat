9ed84bbec4ef11e852a7673bc6e54020ad043854
Mr.Benício <lbeniciosilva@gmail.com>
2025-09-21 20:20:39 -0300

    Update Install-Dependencies.bat

--- Install-Dependencies.bat
+++ Install-Dependencies.bat
@@ -1,7 +1,6 @@
 @echo off 
-echo Instalando dependencias do projeto... 
+echo Instalando dependencias do projeto e iniciando o container Docker...
 
 start cmd /k "cd avicia && npm install" 
 
 start cmd /k "cd docker && docker-compose up -d"
-
