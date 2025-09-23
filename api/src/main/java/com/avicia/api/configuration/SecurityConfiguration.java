f79902ea149777fe7d74faf0fea940faae93e829
rhyan <rhyanlima.dev@gmail.com>
2025-09-22 22:12:25 -0300

    Ajuste: baypass do serviço de autentificação para fins de teste

--- api/src/main/java/com/avicia/api/configuration/SecurityConfiguration.java
+++ api/src/main/java/com/avicia/api/configuration/SecurityConfiguration.java
@@ -48,7 +48,7 @@ public class SecurityConfiguration {
                                                             .requestMatchers(HttpMethod.POST, "/api/admin/login").permitAll()
 
                                                             .requestMatchers(HttpMethod.GET, "/api/teste-jwt").permitAll()
-                                                            .anyRequest().authenticated()
+                                                            .anyRequest().permitAll()
             )
             .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
